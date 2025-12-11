183ec26b60659713f0555b1efd8286b8106bf1d9
Ruben Perez <rubenperez038@gmail.com>
2025-12-11 18:24:02 +0100

    Move to compiled

--- include/nativepg/protocol/read_message_fsm.hpp
+++ include/nativepg/protocol/read_message_fsm.hpp
@@ -14,7 +14,6 @@
 #include <cstdint>
 #include <span>
 
-#include "nativepg/protocol/header.hpp"
 #include "nativepg/protocol/messages.hpp"
 #include "nativepg/protocol/views.hpp"
 
@@ -92,39 +91,7 @@ public:
 
     read_message_fsm() = default;
 
-    result resume(std::span<const unsigned char> data)
-    {
-        if (msg_size_ == -1)
-        {
-            // Header. Ensure we have enough data
-            if (data.size() < 5u)
-                return result(static_cast<std::size_t>(5u - data.size()));
-
-            // Do the parsing
-            auto header_result = parse_header(boost::span<const unsigned char, 5>(data));
-            if (header_result.has_error())
-                return header_result.error();
-
-            // Record the header fields to signal that we're done with the header
-            auto header = *header_result;
-            msg_type_ = header.type;
-            msg_size_ = header.size;
-        }
-
-        // Body. Ensure we have enough data. The header is not discarded
-        // until the message is fully parsed for simplicity,
-        // and the type byte is not included in the length
-        BOOST_ASSERT(msg_size_ != -1);
-        const auto expected_size = static_cast<std::size_t>(msg_size_ + 1);
-        if (data.size() < expected_size)
-            return result(static_cast<std::size_t>(expected_size - data.size()));
-
-        // Do the parsing
-        auto msg_result = parse(msg_type_, data.subspan(5, expected_size - 5));
-        if (msg_result.has_error())
-            return msg_result.error();
-        return result(*msg_result, expected_size);
-    }
+    result resume(std::span<const unsigned char> data);
 };
 
 }  // namespace nativepg::protocol

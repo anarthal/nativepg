9bfdae7b3c1bf51091efb092fb1fcee6de63e43f
Ruben Perez <rubenperez038@gmail.com>
2026-06-09 12:56:55 +0200

    poper doc

--- src/nativepg_internal/check_request.hpp
+++ src/nativepg_internal/check_request.hpp
@@ -15,17 +15,23 @@
 
 namespace nativepg::protocol::detail {
 
+// Checks that a request is well-formed, suitable for execution.
+// Requests might go through intermediate states when being built,
+// but not all of them are suitable for execution.
+// To enable appropriate error recovery, requests need to be
+// composed of units of either:
+//   1. The extended query protocol. This is a set of extended query messages ending with a sync.
+//   2. The simple query protocol. This is a single query message.
+// Such units may be mixed arbitrarily, but can't be broken down
+// (i.e. placing a query in the middle of an extended batch is illegal).
+// Doing this, we can manage errors by counting ready_for_query messages.
+// TODO: do we need to support requests ending with flush? is there any use case for this?
 inline boost::system::error_code check_request(const request& req)
 {
     // Empty requests are not allowed
     if (req.messages().empty())
         return client_errc::empty_request;
 
-    // To enable appropriate error recovery, requests need to either
-    //   1. End with a sync
-    //   2. Be a sequence of query messages, with nothing in front of it
-    //   3. End with a sequence of query messages, with a sync in front of it
-    // TODO: do we need to support requests ending with flush? is there any use case for this?
     bool query_seen = false;
     for (auto it = req.messages().rbegin(); it != req.messages().rend(); ++it)
     {

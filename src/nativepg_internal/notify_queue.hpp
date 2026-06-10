5b5e80391c2ec4985ce478b6a0adecdb0154de6d
Ruben Perez <rubenperez038@gmail.com>
2026-06-10 18:52:15 +0200

    Coalesce connect/disconnect

--- src/nativepg_internal/notify_queue.hpp
+++ src/nativepg_internal/notify_queue.hpp
@@ -57,9 +57,29 @@ public:
     }
 
     // Producer side. Connects and disconnects are not subject to backpressure
-    // TODO: coalesce connect/disconnect events to avoid unlimited resource consumption
     void add_connect() { add_event({notify_event_type::connect}); }
-    void add_disconnect() { add_event({notify_event_type::disconnect}); }
+    void add_disconnect()
+    {
+        // If the last element is a connect, instead of adding a disconnect,
+        // we can just remove the connect. For most purposes (e.g. cache invalidation),
+        // keeping track of every reconnection is not required. Doing this avoids
+        // unbound memory consumption if the user is not reading notifications.
+        // TODO: I think we could avoid losing information by storing connects/disconnects
+        // in a 'compressed' format (e.g. store the int 5 if 5 connect/disconnect cycles happen)
+        // But this would require having != formats in pending_ and the output buffer
+        if (!pending_.empty() && pending_.back().type == notify_event_type::connect)
+        {
+            pending_.pop_back();
+            if (has_space())
+                space_available_.set();
+            if (pending_.empty())
+                events_available_.clear();
+        }
+        else
+        {
+            add_event({notify_event_type::disconnect});
+        }
+    }
 
     // Notifications are subject to backpressure
     bool try_add_notify(const protocol::notification_response& msg)

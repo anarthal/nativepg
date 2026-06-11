f664fd3c19aa9cc11bfe3af116be0736a26c6db3
Ruben Perez <rubenperez038@gmail.com>
2026-06-11 13:30:30 +0200

    api decl

--- include/nativepg/co_connection.hpp
+++ include/nativepg/co_connection.hpp
@@ -8,6 +8,7 @@
 #ifndef NATIVEPG_CO_CONNECTION_HPP
 #define NATIVEPG_CO_CONNECTION_HPP
 
+#include <boost/capy/buffers.hpp>
 #include <boost/capy/concept/executor.hpp>
 #include <boost/capy/ex/execution_context.hpp>
 #include <boost/capy/io/any_stream.hpp>
@@ -15,6 +16,7 @@
 
 #include <concepts>
 #include <memory>
+#include <span>
 
 #include "nativepg/connect_params.hpp"
 #include "nativepg/extended_error.hpp"
@@ -23,6 +25,31 @@
 #include "nativepg/response_handler.hpp"
 
 namespace nativepg {
+
+class exec_some_result
+{
+    enum class type
+    {
+        done,
+        copy_out,
+        copy_out_eof,
+    };
+
+    type type_{type::done};
+    std::span<const boost::capy::const_buffer> copy_out;
+
+public:
+    exec_some_result() noexcept = default;
+    exec_some_result(std::span<const boost::capy::const_buffer> copy_out_buffers, bool is_eof) noexcept
+        : type_(is_eof ? type::copy_out_eof : type::copy_out), copy_out(copy_out_buffers)
+    {
+    }
+
+    bool is_done() const { return type_ == type::done; }
+    bool is_copy_out() const { return type_ == type::copy_out || type_ == type::copy_out_eof; }
+    bool is_copy_out_eof() const { return type_ == type::copy_out_eof; }
+    std::span<const boost::capy::const_buffer> copy_out_data() const { return copy_out; }
+};
 
 class co_connection
 {
@@ -49,6 +76,14 @@ public:
     boost::capy::io_task<> connect(connect_params params, diagnostics* diag = nullptr);
 
     boost::capy::io_task<> exec(
+        const request& req,
+        response_handler_ref handler,
+        diagnostics* diag = nullptr
+    );
+
+    // TODO: I think this could be more ergonomic if we packaged the request and handler
+    // into a generator-like object
+    boost::capy::io_task<exec_some_result> exec_some(
         const request& req,
         response_handler_ref handler,
         diagnostics* diag = nullptr

ccd6496c1b105cda7501e45f4e90b37ab3fbed96
Ruben Perez <rubenperez038@gmail.com>
2026-06-12 19:26:43 +0200

    commit

--- test/unit/protocol/test_read_buffer.cpp
+++ test/unit/protocol/test_read_buffer.cpp
@@ -8,9 +8,8 @@
 #include <boost/core/lightweight_test.hpp>
 
 #include <algorithm>
-#include <cstddef>
-#include <limits>
 #include <span>
+#include <utility>
 
 #include "nativepg/protocol/detail/read_buffer.hpp"
 #include "test_utils.hpp"
@@ -79,10 +78,93 @@ void test_construct_rounds_size()
 }
 
 // Moving, then move-assigning works OK
+void test_move()
+{
+    constexpr unsigned char data[] = {10, 20, 30, 40};
+
+    // A buffer with some committed data
+    read_buffer buff{64u};
+    copy_to(data, buff.prepared_area());
+    buff.commit(4u);
+
+    // Move-construct: the committed data and free space transfer to the new buffer
+    read_buffer moved{std::move(buff)};
+    NATIVEPG_TEST_CONT_EQ(moved.committed_area(), data);
+    BOOST_TEST_EQ(moved.prepared_area().size(), 60u);
+
+    // Move-assign into a different buffer: same transfer, old contents are released
+    read_buffer assigned{8u};
+    assigned = std::move(moved);
+    NATIVEPG_TEST_CONT_EQ(assigned.committed_area(), data);
+    BOOST_TEST_EQ(assigned.prepared_area().size(), 60u);
+}
+
 // Committing the entire prepared area works
+void test_commit_entire_prepared_area()
+{
+    constexpr unsigned char data[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    // A buffer exactly the size of the data we'll write
+    read_buffer buff{8u};
+    copy_to(data, buff.prepared_area());
+
+    // Commit all of it: everything moves to the committed area, no free space left
+    buff.commit(8u);
+    NATIVEPG_TEST_CONT_EQ(buff.committed_area(), data);
+    BOOST_TEST_EQ(buff.prepared_area().size(), 0u);
+}
+
 // Committing size > prepared area commits the entire prepared area
+void test_commit_over_prepared_area()
+{
+    constexpr unsigned char data[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    // Populate buffer
+    read_buffer buff{8u};
+    copy_to(data, buff.prepared_area());
+
+    // Committing more than is available is clamped to the whole prepared area
+    buff.commit(100u);
+    NATIVEPG_TEST_CONT_EQ(buff.committed_area(), data);
+    BOOST_TEST_EQ(buff.prepared_area().size(), 0u);
+}
+
 // Committing size == 0 is a no-op
+void test_commit_zero()
+{
+    constexpr unsigned char data[] = {1, 2, 3, 4};
+
+    // Setup
+    read_buffer buff{16u};
+    copy_to(data, buff.prepared_area());
+    buff.commit(4u);
+
+    // Committing 0 leaves both areas untouched
+    buff.commit(0u);
+    NATIVEPG_TEST_CONT_EQ(buff.committed_area(), data);
+    BOOST_TEST_EQ(buff.prepared_area().size(), 12u);
+}
+
 // Two consecutive commits are OK
+void test_two_commits()
+{
+    constexpr unsigned char data[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    read_buffer buff{16u};
+
+    // First chunk
+    copy_to(std::span(data).first(5), buff.prepared_area());
+    buff.commit(5u);
+
+    // Second chunk goes into the now-shrunk prepared area, right after the committed bytes
+    copy_to(std::span(data).last(3), buff.prepared_area());
+    buff.commit(3u);
+
+    // Both chunks form a single contiguous committed area
+    NATIVEPG_TEST_CONT_EQ(buff.committed_area(), data);
+    BOOST_TEST_EQ(buff.prepared_area().size(), 8u);
+}
+
 // Consuming the entire committed area works
 // Consuming size > committed area consumes the entire committed area
 // Consuming size == 0 is a no-op
@@ -106,6 +188,11 @@ int main()
     test_usual_workflow();
     test_construct_zero_size();
     test_construct_rounds_size();
+    test_move();
+    test_commit_entire_prepared_area();
+    test_commit_over_prepared_area();
+    test_commit_zero();
+    test_two_commits();
 
     return boost::report_errors();
 }

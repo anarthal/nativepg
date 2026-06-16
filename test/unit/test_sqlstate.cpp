038b3d8d3f052049e4cd56db86eb85fa71c78356
Ruben Perez <rubenperez038@gmail.com>
2026-06-16 11:02:38 +0200

    errors

--- test/unit/test_sqlstate.cpp
+++ test/unit/test_sqlstate.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/core/lightweight_test.hpp>
 
+#include <string_view>
 #include <system_error>
 
 #include "nativepg/sqlstate.hpp"
@@ -46,11 +47,49 @@ void test_runtime_parsing_success()
     BOOST_TEST_EQ(parse_sqlstate("HV00B"), std::error_code(293339147, cat));
 }
 
+// On error, unknown_sqlstate (error_code -1) is returned
+void test_runtime_parsing_error()
+{
+    const std::error_code invalid(-1, get_sqlstate_category());
+
+    // empty string
+    BOOST_TEST_EQ(parse_sqlstate(""), invalid);
+
+    // string has < 5 chars
+    BOOST_TEST_EQ(parse_sqlstate("0000"), invalid);
+
+    // string has > 5 chars
+    BOOST_TEST_EQ(parse_sqlstate("000000"), invalid);
+
+    // string has lowercase chars
+    BOOST_TEST_EQ(parse_sqlstate("0a000"), invalid);
+
+    // string has punctuation
+    BOOST_TEST_EQ(parse_sqlstate("0000-"), invalid);
+    BOOST_TEST_EQ(parse_sqlstate("42:01"), invalid);
+
+    // string has space/newline/control
+    BOOST_TEST_EQ(parse_sqlstate("00 00"), invalid);
+    BOOST_TEST_EQ(parse_sqlstate("00\n00"), invalid);
+    BOOST_TEST_EQ(parse_sqlstate("00\t00"), invalid);
+
+    // string has NULL characters
+    const char with_null[] = {'2', '3', '\0', '0', '5'};
+    BOOST_TEST_EQ(parse_sqlstate(std::string_view(with_null, 5)), invalid);
+
+    // string has UTF-8 surrogates
+    BOOST_TEST_EQ(parse_sqlstate(std::string_view("val\xc3\xb1", 5)), invalid);
+
+    // string has 0xff chars
+    BOOST_TEST_EQ(parse_sqlstate(std::string_view("0000\xff", 5)), invalid);
+}
+
 }  // namespace
 
 int main()
 {
     test_runtime_parsing_success();
+    test_runtime_parsing_error();
 
     return boost::report_errors();
 }

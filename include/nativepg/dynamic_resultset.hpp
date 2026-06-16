f7e57f99b01bee815ed6d39635386ca10f65984a
Ruben Perez <rubenperez038@gmail.com>
2026-06-16 18:38:30 +0200

    implement dynamic_resultset

--- include/nativepg/dynamic_resultset.hpp
+++ include/nativepg/dynamic_resultset.hpp
@@ -16,10 +16,14 @@
 #include <iterator>
 #include <optional>
 #include <span>
+#include <string_view>
 #include <vector>
 
+#include "nativepg/protocol/command_complete.hpp"
 #include "nativepg/protocol/common.hpp"
+#include "nativepg/protocol/data_row.hpp"
 #include "nativepg/protocol/describe.hpp"
+#include "nativepg/protocol/execute.hpp"
 #include "nativepg/protocol/views.hpp"
 
 namespace nativepg {
@@ -38,6 +42,11 @@ struct offset_and_length
             return std::nullopt;
         return std::span<const unsigned char>{data + offset, length};
     }
+
+    std::string_view to_string_view(const unsigned char* data) const
+    {
+        return {reinterpret_cast<const char*>(data) + offset, length};
+    }
 };
 
 // Like protocol::field_description, but strings are offset based
@@ -54,7 +63,7 @@ struct offsetted_field_description
     protocol::field_description to_field_description(const unsigned char* data) const
     {
         return {
-            .name = {reinterpret_cast<const char*>(data + name.offset), name.length},
+            .name = name.to_string_view(data),
             .table_oid = table_oid,
             .column_attribute = column_attribute,
             .type_oid = type_oid,
@@ -432,11 +441,85 @@ class dynamic_resultset
     std::vector<detail::offsetted_field_description> field_descr_;
     std::vector<detail::offset_and_length> values_;
     std::vector<unsigned char> data_;
+    detail::offset_and_length command_complete_tag_{};
+    bool portal_suspended_{false};
+
+    detail::offset_and_length insert_data(std::span<const unsigned char> value)
+    {
+        detail::offset_and_length res{.offset = data_.size(), .length = value.size()};
+        data_.insert(data_.end(), value.begin(), value.end());
+        return res;
+    }
+
+    detail::offset_and_length insert_data(std::string_view value)
+    {
+        return insert_data(
+            std::span<const unsigned char>{reinterpret_cast<const unsigned char*>(value.data()), value.size()}
+        );
+    }
 
 public:
     dynamic_resultset() = default;
 
+    void clear()
+    {
+        field_descr_.clear();
+        values_.clear();
+        data_.clear();
+        command_complete_tag_ = {};
+        portal_suspended_ = false;
+    }
+
+    // TODO: these methods should be private?
+    void add_row_description(const protocol::row_description& row_descr)
+    {
+        field_descr_.clear();
+        for (const auto& descr : row_descr.field_descriptions)
+        {
+            field_descr_.push_back({
+                .name = insert_data(descr.name),
+                .table_oid = descr.table_oid,
+                .column_attribute = descr.column_attribute,
+                .type_oid = descr.type_oid,
+                .type_length = descr.type_length,
+                .type_modifier = descr.type_modifier,
+                .fmt_code = descr.fmt_code,
+            });
+        }
+    }
+
+    void add_row(const protocol::data_row& row)
+    {
+        // TODO: I think this assert can technically trigger,
+        // but it may not be this class' responsibility
+        BOOST_ASSERT(row.columns.size() == field_descr_.size());
+        for (const auto& value : row.columns)
+        {
+            if (value.has_value())
+                values_.push_back(insert_data(*value));
+            else
+                values_.push_back({.offset = 0u, .length = static_cast<std::size_t>(-1)});
+        }
+    }
+
+    void add_command_complete(protocol::command_complete msg)
+    {
+        command_complete_tag_ = insert_data(msg.tag);
+    }
+
+    void add_portal_suspended(protocol::portal_suspended) { portal_suspended_ = true; }
+
     field_descriptions_view field_descriptions() const noexcept { return {field_descr_, data_.data()}; }
+    rows_view rows() const noexcept { return {values_, field_descr_.size(), data_.data()}; }
+
+    // This is equivalent to PQcmdStatus
+    // TODO: this tag is what libpq uses for PQcmdTuples and PQoidValue,
+    // implement functions to parse it
+    std::string_view command_complete_tag() const noexcept
+    {
+        return command_complete_tag_.to_string_view(data_.data());
+    }
+    bool portal_suspended() const noexcept { return portal_suspended_; }
 };
 
 }  // namespace nativepg

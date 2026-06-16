f02668cae8bc8e69409cfa1a6b0db140e1683472
Ruben Perez <rubenperez038@gmail.com>
2026-06-16 20:14:59 +0200

    Finished tests

--- test/unit/test_field_view.cpp
+++ test/unit/test_field_view.cpp
@@ -16,7 +16,7 @@ using namespace nativepg;
 
 namespace {
 
-// Default ctor builds a NULL value
+// Default ctor builds a NULL value. data() returns an empty span
 void test_default_constructor()
 {
     field_view f;
@@ -24,13 +24,44 @@ void test_default_constructor()
     NATIVEPG_TEST_CONT_EQ(f.data(), std::span<const unsigned char>{});
 }
 
-// Constructor
+// Constructor from actual data
+void test_constructor_from_data()
+{
+    const unsigned char data[] = {1, 2, 3, 4};
+    field_view f{std::span<const unsigned char>(data)};
+    BOOST_TEST(!f.is_null());
+    NATIVEPG_TEST_CONT_EQ(f.data(), data);
+}
+
+// Constructor from a default-constructed span:
+// interpreted as an empty value, not as a NULL
+void test_constructor_from_default_span()
+{
+    field_view f{std::span<const unsigned char>{}};
+    BOOST_TEST(!f.is_null());
+    NATIVEPG_TEST_CONT_EQ(f.data(), std::span<const unsigned char>{});
+}
+
+// Constructor from a span with zero size but (possibly) non-null data():
+// interpreted as an empty value, not as a NULL
+// (sanity check, we don't have data != nullptr as a NULL indicator)
+void test_constructor_from_empty_nonnull_span()
+{
+    const unsigned char data[] = {1, 2, 3, 4};
+    std::span<const unsigned char> empty_nonnull(data, 0u);
+    field_view f{empty_nonnull};
+    BOOST_TEST(!f.is_null());
+    NATIVEPG_TEST_CONT_EQ(f.data(), std::span<const unsigned char>{});
+}
 
 }  // namespace
 
 int main()
 {
     test_default_constructor();
+    test_constructor_from_data();
+    test_constructor_from_default_span();
+    test_constructor_from_empty_nonnull_span();
 
     return boost::report_errors();
 }

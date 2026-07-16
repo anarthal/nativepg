61d4cc14f80010c7988c28ab765a25e831b53715
Ruben Perez <rubenperez038@gmail.com>
2026-07-16 12:29:56 +0200

    Adds an example of using affected_rows

---
+++ example/update_affected_rows.cpp
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 2025 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/capy/ex/run_async.hpp>
+#include <boost/capy/task.hpp>
+#include <boost/corosio/io_context.hpp>
+#include <boost/describe/class.hpp>
+
+#include <iostream>
+#include <string_view>
+#include <vector>
+
+#include "nativepg/co_connection.hpp"
+#include "nativepg/extended_error.hpp"
+#include "nativepg/request.hpp"
+#include "nativepg/response.hpp"
+
+using namespace nativepg;
+namespace capy = boost::capy;
+namespace corosio = boost::corosio;
+
+// TODO: this should probably use the dynamic API
+struct empty
+{
+    std::int32_t f3;
+    std::string f1;
+};
+BOOST_DESCRIBE_STRUCT(empty, (), ())
+
+static void print_err(const char* prefix, std::error_code err, const diagnostics& diag)
+{
+    std::cout << prefix << ": " << err << ": " << err.message();
+    if (!diag.message().empty())
+        std::cout << ": " << diag.message();
+    std::cout << '\n';
+}
+
+static capy::task<> co_main()
+{
+    // Create a connection
+    co_connection conn{co_await capy::this_coro::executor};
+    diagnostics diag;
+
+    // Connect
+    auto [ec] = co_await conn.connect(
+        {.hostname = "localhost", .username = "postgres", .password = "secret", .database = "postgres"},
+        &diag
+    );
+    if (ec)
+    {
+        print_err("Error connecting", ec, diag);
+        co_return;
+    }
+    std::cout << "Startup complete\n";
+
+    // Compose our request
+    request req;
+    req.add_query("UPDATE myt SET f3 = f3 + 1 WHERE f1 <> $1", {"hehe"});
+
+    // Structures to parse the response into
+    std::vector<empty> vec;
+    command_info info;
+
+    auto [ec2] = co_await conn.exec(req, into(vec, &info), &diag);
+    if (ec2)
+    {
+        print_err("Error inserting", ec, diag);
+        co_return;
+    }
+
+    std::cout << "Affected: " << info.affected_rows.value_or(0u) << " rows\n";
+}
+
+int main()
+{
+    // The I/O context, required for all I/O operations
+    corosio::io_context ctx;
+
+    // Schedules the main coroutine for execution
+    capy::run_async(
+        ctx.get_executor(),
+        []() {
+           // Runs when the main coroutine finishes normally
+           std::cout << "Done\n";
+        },
+        [](std::exception_ptr exc) {
+            // Runs when the main coroutine finishes with an exception
+            try {
+               std::rethrow_exception(exc);
+            } catch (const std::exception& e) {
+               std::cerr << "Error: " << e.what() << std::endl;
+            }
+            exit(1);
+        }
+    )(co_main());
+
+    // Executes all pending work, including the main coroutine
+    ctx.run();
+}

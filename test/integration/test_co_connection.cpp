f0934b3fa3d2d54184194599127d0847c6b5529b
Anarthal (Rubén Pérez) <34971811+anarthal@users.noreply.github.com>
2026-07-20 19:06:00 +0200

    Adds basic integration testing (#66)

---
+++ test/integration/test_co_connection.cpp
@@ -0,0 +1,84 @@
+//
+// Copyright (c) 2025 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/assert/source_location.hpp>
+#include <boost/capy/ex/run_async.hpp>
+#include <boost/capy/ex/this_coro.hpp>
+#include <boost/capy/io_result.hpp>
+#include <boost/capy/task.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <boost/corosio/io_context.hpp>
+#include <boost/describe/class.hpp>
+#include <boost/describe/operators.hpp>
+
+#include <string>
+#include <vector>
+
+#include "ci_server.hpp"
+#include "corosio_utils.hpp"
+#include "nativepg/co_connection.hpp"
+#include "nativepg/extended_error.hpp"
+#include "nativepg/request.hpp"
+#include "nativepg/response.hpp"
+#include "printing.hpp"
+
+namespace capy = boost::capy;
+using namespace nativepg;
+using namespace nativepg::test;
+
+namespace {
+
+struct row_int
+{
+    int value;
+};
+BOOST_DESCRIBE_STRUCT(row_int, (), (value))
+
+struct row_string
+{
+    std::string value;
+};
+BOOST_DESCRIBE_STRUCT(row_string, (), (value))
+
+using boost::describe::operators::operator==;
+using boost::describe::operators::operator<<;
+
+// Exec (potentially with pipelining) works
+capy::task<> test_exec_success()
+{
+    // Setup
+    diagnostics diag;
+    co_connection conn{co_await capy::this_coro::executor};
+    if (!check_success(co_await conn.connect(default_connect_params(), &diag), diag))
+        co_return;
+
+    // Request and response
+    request req;
+    req.add_query("SELECT $1 + $2 AS value", {42, 10});
+    req.add_query("SELECT $1 AS value", {"abcd"});
+    std::vector<row_int> ints;
+    std::vector<row_string> strings;
+
+    // Execute
+    if (!check_success(co_await conn.exec(req, response{into(ints), into(strings)}, &diag), diag))
+        co_return;
+
+    // Check
+    std::vector<row_int> ints_expected{{.value = 52}};
+    std::vector<row_string> strings_expected{{.value = "abcd"}};
+    BOOST_TEST_ALL_EQ(ints.begin(), ints.end(), ints_expected.begin(), ints_expected.end());
+    BOOST_TEST_ALL_EQ(strings.begin(), strings.end(), strings_expected.begin(), strings_expected.end());
+}
+
+}  // namespace
+
+int main()
+{
+    run_coroutine_test(test_exec_success());
+
+    return boost::report_errors();
+}
---
+++ test/test_utils/corosio_utils.cpp
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2025 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/assert/source_location.hpp>
+#include <boost/capy/ex/run_async.hpp>
+#include <boost/capy/task.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <boost/corosio/io_context.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <system_error>
+#include <utility>
+
+#include "corosio_utils.hpp"
+#include "printing.hpp"
+
+void nativepg::test::run_coroutine_test(boost::capy::task<void> test, boost::source_location loc)
+{
+    // Set a timeout to the tests, so they don't hang on error
+    constexpr std::chrono::seconds test_timeout{10};
+    bool finished = false;
+    auto wrapper_fn = [test = std::move(test), &finished]() mutable -> boost::capy::task<void> {
+        co_await std::move(test);
+        finished = true;
+    };
+
+    // Actually run the test
+    boost::corosio::io_context ctx;
+    boost::capy::run_async(ctx.get_executor())(wrapper_fn());
+    ctx.run_for(test_timeout);
+
+    // Check that it finished
+    if (!BOOST_TEST(finished))
+        std::cerr << "  Called from " << loc << std::endl;
+}
+
+bool nativepg::test::check_success(std::error_code ec, const diagnostics& diag, boost::source_location loc)
+{
+    bool ok = BOOST_TEST_EQ(ec, std::error_code());
+    ok = BOOST_TEST_EQ(diag, diagnostics()) && ok;
+    if (!ok)
+        std::cerr << "  Called from " << loc << std::endl;
+    return ok;
+}

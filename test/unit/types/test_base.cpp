d87825d464784664d9a406b93b1020ad432b4542
Henry Roeland <henry@roeland.net>
2026-07-22 19:06:56 +0200

    Fixed merge conflicts, compiler warnings, and bug.

--- test/unit/types/test_base.cpp
+++ test/unit/types/test_base.cpp
@@ -23,7 +23,6 @@
 #include "nativepg/detail/field_traits.hpp"
 #include "nativepg/protocol/describe.hpp"
 #include "nativepg/types/base.hpp"
-#include "test_utils.hpp"
 
 using namespace nativepg;
 using boost::system::error_code;
@@ -830,20 +829,6 @@ void test_field_parse_int32_from_int2_wire_success()
     // Assert
     BOOST_TEST_EQ(err, boost::system::errc::success);
     BOOST_TEST_EQ(out_val, 42);
-}
-
-void test_field_parse_unexpected_null_error()
-{
-    // Arrange
-    bool b = false;
-    field_view fv;  // NULL
-    const auto desc = make_field_description(detail::bool_oid);
-
-    // Act
-    auto err = detail::field_parse<bool>::call(fv, desc, b);
-
-    // Assert
-    BOOST_TEST_EQ(err, error_code(client_errc::unexpected_null));
 }
 
 }  // namespace

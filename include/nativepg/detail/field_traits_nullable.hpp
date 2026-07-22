b58e2d75af3f2b8b21412c7fdb3082b0dcce00f4
Anarthal (Rubén Pérez) <34971811+anarthal@users.noreply.github.com>
2026-07-22 08:12:40 +0200

    Cleans up nullable traits (#68)

--- include/nativepg/detail/field_traits_nullable.hpp
+++ include/nativepg/detail/field_traits_nullable.hpp
@@ -11,14 +11,26 @@
 #include <boost/system/error_code.hpp>
 
 #include <optional>
-#include <span>
-#include <string>
-#include <utility>
+#include <type_traits>
 
+#include "nativepg/field_view.hpp"
 #include "nativepg/protocol/describe.hpp"
-#include "nativepg/types/nullable.hpp"
 
 namespace nativepg::detail {
+
+// Is a type an optional?
+template <class T>
+struct is_optional : std::false_type
+{
+};
+
+template <class T>
+struct is_optional<std::optional<T>> : std::true_type
+{
+};
+
+template <class T>
+inline constexpr auto is_optional_v = is_optional<T>::value;
 
 // --- Is a type compatible with what we get from DB?
 template <class T>
@@ -28,7 +40,7 @@ template <typename T>
 struct field_is_compatible<std::optional<T>>
 {
     static_assert(
-        !types::is_nullable_type_v<T>,
+        !is_optional_v<T>,
         "Nested std::optional (e.g. std::optional<std::optional<T>>) is not supported"
     );
 
@@ -46,12 +58,12 @@ template <typename T>
 struct field_parse<std::optional<T>>
 {
     static_assert(
-        !types::is_nullable_type_v<T>,
+        !is_optional_v<T>,
         "Nested std::optional (e.g. std::optional<std::optional<T>>) is not supported"
     );
 
     static boost::system::error_code call(
-        const field_view& from,
+        field_view from,
         const protocol::field_description& desc,
         std::optional<T>& to
     )

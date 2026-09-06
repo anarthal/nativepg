3b71d16ba745217e7f6d36246a91955ce091d47c
Ruben Perez <rubenperez038@gmail.com>
2026-09-06 14:44:26 +0200

    Move to compiled

--- test/test_utils/include/test_utils/response_msg_type.hpp
+++ test/test_utils/include/test_utils/response_msg_type.hpp
@@ -9,33 +9,12 @@
 #define NATIVEPG_TEST_RESPONSE_MSG_TYPE_HPP
 
 #include <cstddef>
-#include <ostream>
+#include <iosfwd>
 
 #include "nativepg/responses/any_request_message.hpp"
 #include "nativepg/responses/response_handler.hpp"
 
 namespace nativepg::test {
-
-inline const char* to_string(any_request_message::kind value)
-{
-    using kind = any_request_message::kind;
-
-    switch (value)
-    {
-        case kind::bind_complete: return "bind_complete";
-        case kind::close_complete: return "close_complete";
-        case kind::command_complete: return "command_complete";
-        case kind::data_row: return "data_row";
-        case kind::parameter_description: return "parameter_description";
-        case kind::row_description: return "row_description";
-        case kind::empty_query_response: return "empty_query_response";
-        case kind::portal_suspended: return "portal_suspended";
-        case kind::error_response: return "error_response";
-        case kind::parse_complete: return "parse_complete";
-        case kind::message_skipped: return "message_skipped";
-        default: return "<unknown any_request_message::kind>";
-    }
-}
 
 struct on_msg_args
 {
@@ -43,11 +22,9 @@ struct on_msg_args
     std::size_t offset;
 
     friend bool operator==(const on_msg_args&, const on_msg_args&) = default;
-    friend std::ostream& operator<<(std::ostream& os, const on_msg_args& v)
-    {
-        return os << "{ " << to_string(v.type) << ", " << v.offset << " }";
-    }
 };
+
+std::ostream& operator<<(std::ostream& os, const on_msg_args& v);
 
 // Calls on_message and returns the produced error
 template <response_handler Handler>

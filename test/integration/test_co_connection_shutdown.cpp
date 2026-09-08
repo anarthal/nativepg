54749dbdd6ac5a60f88e766f56566c6d8076feaa
Ruben Perez <rubenperez038@gmail.com>
2026-09-08 12:17:37 +0200

    Simplifies test

--- test/integration/test_co_connection_shutdown.cpp
+++ test/integration/test_co_connection_shutdown.cpp
@@ -38,7 +38,7 @@ capy::task<> test_shutdown()
     // Sanity check: the connection works before shutting it down
     request req;
     req.add_simple_query("SELECT 1");
-    if (!check_success(co_await conn.exec(req, check_execute(), &diag), diag))
+    if (!check_success(co_await conn.exec(req, check(), &diag), diag))
         co_return;
 
     // Shut the connection down. This should succeed
@@ -46,39 +46,13 @@ capy::task<> test_shutdown()
     BOOST_TEST_EQ(shutdown_ec, std::error_code());
 
     // exec no longer works
-    auto [exec_ec] = co_await conn.exec(req, check_execute());
+    auto [exec_ec] = co_await conn.exec(req, check());
     BOOST_TEST_NE(exec_ec, std::error_code());
-}
 
-// Regression test: after shutdown, the connection can be re-opened,
-// even if there were unread bytes in the network buffer.
-capy::task<> test_shutdown_reopen_pending_bytes()
-{
-    // Setup
-    diagnostics diag;
-    co_connection conn{co_await capy::this_coro::executor};
+    // The connection can be re-opened
     if (!check_success(co_await conn.connect(default_connect_params(), &diag), diag))
         co_return;
-
-    // Produce a NotificiationResponse that won't be read yet.
-    request req;
-    req.add_simple_query("LISTEN my_channel");
-    req.add_simple_query("NOTIFY my_channel");
-    if (!check_success(co_await conn.exec(req, check_execute(), &diag), diag))
-        co_return;
-
-    // Shut the connection down
-    auto [shutdown_ec] = co_await conn.shutdown();
-    BOOST_TEST_EQ(shutdown_ec, std::error_code());
-
-    // We can re-establish the connection
-    if (!check_success(co_await conn.connect(default_connect_params(), &diag), diag))
-        co_return;
-
-    // The connection works
-    request req_select;
-    req_select.add_simple_query("SELECT 1");
-    if (!check_success(co_await conn.exec(req_select, check_execute(), &diag), diag))
+    if (!check_success(co_await conn.exec(req, check(), &diag), diag))
         co_return;
 }
 
@@ -87,7 +61,6 @@ capy::task<> test_shutdown_reopen_pending_bytes()
 int main()
 {
     run_coroutine_test(test_shutdown());
-    run_coroutine_test(test_shutdown_reopen_pending_bytes());
 
     return boost::report_errors();
 }

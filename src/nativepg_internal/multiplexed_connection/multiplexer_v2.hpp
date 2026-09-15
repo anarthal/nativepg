f6306e2507289bcf274d43c13ea09a4d22074fc3
Ruben Perez <rubenperez038@gmail.com>
2026-09-15 18:05:28 +0200

    slightly refined impl

--- src/nativepg_internal/multiplexed_connection/multiplexer_v2.hpp
+++ src/nativepg_internal/multiplexed_connection/multiplexer_v2.hpp
@@ -14,6 +14,7 @@
 #include <boost/capy/io/any_stream.hpp>
 #include <boost/capy/io_task.hpp>
 #include <boost/capy/when_all.hpp>
+#include <boost/capy/when_any.hpp>
 #include <boost/capy/write.hpp>
 #include <boost/intrusive/list.hpp>
 #include <boost/intrusive/list_hook.hpp>
@@ -21,6 +22,8 @@
 #include <cstddef>
 #include <deque>
 #include <iterator>
+#include <system_error>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -117,11 +120,22 @@ struct multiplexer_state
     std::vector<unsigned char> pending_writes;
     read_queue_t read_queue_;
 
-    // This is the writer side of exec, and should be called with the mutex acquired
-    boost::capy::io_task<> write_request(boost::capy::any_stream& stream, const request& req)
-    {
-        BOOST_ASSERT(write_mtx.is_locked());
+    static std::size_t count_rfqs(const request&);
 
+    enum class write_result
+    {
+        nothing_written,
+        partial_write,
+        full_write,
+    };
+
+    // This is the writer side of exec, and should be called with the mutex acquired
+    boost::capy::io_task<std::tuple<std::error_code, write_result>> write_request(
+        boost::capy::any_stream& stream,
+        boost::capy::async_mutex::lock_guard guard,  // forcibly release the mutex when the fn exits
+        const request& req
+    )
+    {
         // If there are any half-written previous requests, write these first.
         // We could use vectored I/O here, but this is a very rare case and optimizing
         // for it is not worth it.
@@ -133,8 +147,11 @@ struct multiplexer_state
                 // There was an error or cancellation
                 pending_writes.erase(pending_writes.begin(), pending_writes.begin() + bytes);
 
-                // TODO: how do we report this?
-                co_return {};
+                // To all effects, nothing was written - we expect no response for this request
+                co_return {
+                    {},
+                    {ec, write_result::nothing_written}
+                };
             }
 
             // We wrote what was remaining of the previous message
@@ -143,35 +160,69 @@ struct multiplexer_state
 
         // Now, our own message
         auto [ec, bytes] = co_await boost::capy::write(stream, boost::capy::make_buffer(req.payload()));
-        if (ec)
-        {
-            // There was an error or cancellation. If we got an incomplete write,
-            // we need to store what we didn't write so the connection doesn't break
-            if (bytes < req.payload().size())
-            {
-                pending_writes.assign(req.payload().begin() + bytes, req.payload().end());
-            }
 
-            // TODO: how do we report this?
-            co_return {};
+        // There was a short write, probably due to an error or cancellation.
+        // We need to store what we didn't write so the connection doesn't break
+        if (bytes < req.payload().size())
+        {
+            pending_writes.assign(req.payload().begin() + bytes, req.payload().end());
+            co_return {
+                {},
+                {ec, write_result::partial_write}
+            };
         }
 
-        // Done and succeeded
-        co_return {};
+        // Everything written (notice that we could, in principle, get an error here)
+        co_return {
+            {},
+            {ec, write_result::full_write}
+        };
     }
 
-    // This is the reader side
-    boost::capy::io_task<> read_response(
+    struct reader_result
+    {
+        std::error_code ec;
+        std::size_t remaining_previous_rfqs;
+        std::size_t remaining_rfqs;
+    };
+
+    // This is the reader side.
+    // Returns the number of ReadyForQuery messages
+    // that were left unread, so proper cleanup may happen
+    boost::capy::io_task<reader_result> read_response(
         boost::capy::any_stream& stream,
-        pending_read* entry,
+        pending_read& handle,
+        bool is_first,
         const request& req,
         response_handler_ref handler
     )
     {
-        // If someone else is writing, we need to wait for our turn
-        if (is_reading)
+        if (!is_first)
         {
+            // We need to wait
+            if (auto [ec] = co_await handle.evt.wait(); ec)
+            {
+                // We were cancelled before having any chance to run
+                co_return {
+                    {},
+                    {
+                     .ec = ec,
+                     .remaining_previous_rfqs = handle.previous_rfqs,
+                     .remaining_rfqs = count_rfqs(req),
+                     }
+                };
+            }
         }
+
+        co_return co_await do_read_response(stream, req, handler);
+    }
+
+    boost::capy::io_task<reader_result> do_read_response(
+        boost::capy::any_stream& stream,
+        const request& req,
+        response_handler_ref handler
+    )
+    {
     }
 
     boost::capy::io_task<> exec(
@@ -185,21 +236,26 @@ struct multiplexer_state
         if (ec)
             co_return {ec};
 
-        // We're in the critical section. Decide whether we're going to read or wait
-        boost::capy::async_event read_evt;
-        pending_read entry{.evt = &read_evt};
-        bool should_read = !is_reading;
-        if (is_reading)
-        {
-            pending_reads.push_back(entry);
-        }
-        else
-        {
-            is_reading = true;
-        }
+        // Add ourselves to the read queue, if required.
+        // This must happen under the mutex to prevent race conditions.
+        // This must happen before writing, because responses
+        //   might start arriving before the full write op finishes.
+        pending_read handle;
+        bool is_first = read_queue_.enter(handle);
 
-        //
-        auto res = co_await boost::capy::when_any()
+        // TODO: this should really be in parallel. Serial for now for simplicity
+        auto [dummy1, writer_res] = co_await write_request(stream, std::move(guard), req);
+        auto [dummy2, reader_res] = co_await read_response(stream, handle, is_first, req, handler);
+
+        // Do the cleanup
+        const std::size_t remaining_rfqs = std::get<1>(writer_res) == write_result::nothing_written
+                                               ? reader_res.remaining_previous_rfqs
+                                               : reader_res.remaining_previous_rfqs +
+                                                     reader_res.remaining_rfqs;
+        read_queue_.exit(handle, remaining_rfqs);
+
+        // TODO: retval
+        co_return {};
     }
 };
 
